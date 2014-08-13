2e6d006e8050c4d95bea7590080863e819e063eb
phpor <phpor@sina.cn>
2014-08-13 15:12:14 +0800

    Update diff.go

--- rdb/decoder/diff.go
+++ rdb/decoder/diff.go
@@ -7,42 +7,42 @@ import (
 	"github.com/phpor/go/rdb"
 )
 
-type diff struct {
+type Diff struct {
 	db int
 	i  int
-	rdb.Nopdiff
+	rdb.NopDecoder
 }
 
-func (p *diff) StartDatabase(n int) {
+func (p *Diff) StartDatabase(n int) {
 	p.db = n
 }
 
-func (p *diff) Set(key, value []byte, expiry int64) {
+func (p *Diff) Set(key, value []byte, expiry int64) {
 	fmt.Printf("db=%d %q -> %q\n", p.db, key, value)
 }
 
-func (p *diff) Hset(key, field, value []byte) {
+func (p *Diff) Hset(key, field, value []byte) {
 	fmt.Printf("db=%d %q . %q -> %q\n", p.db, key, field, value)
 }
 
-func (p *diff) Sadd(key, member []byte) {
+func (p *Diff) Sadd(key, member []byte) {
 	fmt.Printf("db=%d %q { %q }\n", p.db, key, member)
 }
 
-func (p *diff) StartList(key []byte, length, expiry int64) {
+func (p *Diff) StartList(key []byte, length, expiry int64) {
 	p.i = 0
 }
 
-func (p *diff) Rpush(key, value []byte) {
+func (p *Diff) Rpush(key, value []byte) {
 	fmt.Printf("db=%d %q[%d] -> %q\n", p.db, key, p.i, value)
 	p.i++
 }
 
-func (p *diff) Zadd(key []byte, score float64, member []byte) {
+func (p *Diff) Zadd(key []byte, score float64, member []byte) {
 	fmt.Printf("db=%d %q[%d] -> {%q, score=%g}\n", p.db, key, p.i, member, score)
 	p.i++
 }
 
-func (p *diff) StartZSet(key []byte, cardinality, expiry int64) {
+func (p *Diff) StartZSet(key []byte, cardinality, expiry int64) {
 	p.i = 0
 }
