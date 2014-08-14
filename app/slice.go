eb129e29405f3f72452634c03211cfcd372c7db8
phpor <phpor@sina.cn>
2014-08-14 18:10:12 +0800

    1. 了解了一点儿tcp 2. 了解了一下slice

---
+++ app/slice.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main(){
+	var s = "hello_world"
+	fmt.Println(s[:5])
+	fmt.Println(s[5:6])
+	fmt.Println(s[6:])
+}
---
+++ app/tcp.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net"
+	"fmt"
+	"os"
+	"bufio"
+)
+
+func main() {
+
+	conn, err := net.Dial("tcp4", "www.baidu.com:80")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+	reader := bufio.NewReader(conn)
+	buf := make([]byte, 1024)
+	for {
+		n, err := reader.Read(buf)
+		if err != nil || n == 0 {
+			break
+		}
+		fmt.Print(string(buf))
+	}
+}
