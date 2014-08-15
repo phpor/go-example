873d8b5fd6c100d9527d09c30c1ae19a5c3abb6d
phpor <phpor@sina.cn>
2014-08-15 20:00:48 +0800

    1. 了解一些slice的实现

--- app/slice.go
+++ app/slice.go
@@ -1,10 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
+func slice_is_readonly(ss []string) {
+	fmt.Printf("ss in func: %p\n", ss)
+}
 func main(){
-	var s = "hello_world"
-	fmt.Println(s[:5])
+	s := "hello_world"
+	fmt.Println(s[:5])	//字符串默认可以当slice使用
 	fmt.Println(s[5:6])
 	fmt.Println(s[6:])
+
+
+
+	ss := []string{"hello", "word"}
+	fmt.Printf("ss in main: %p\n", ss)
+	fmt.Printf("ss[1:] in main: %p\n", ss[1:])
+	slice_is_readonly(ss[1:])
+
+	sss := []string{"你好", "世界"}
+	fmt.Println(sss[1:])
+	fmt.Println(sss[:1])
+	fmt.Println("golang 默认没有字符集转换类库，需要第三方类库")
+
 }
