a94cbf508b55480c20f3487c92a8003e65d0afe8
phpor <phpor@sina.cn>
2014-08-15 19:33:35 +0800

    1. 用go写模块时，如何只提供 .a 文件，不提供src 文件啊

---
+++ app/uselibtest.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/phpor/goexample/lib/test"
+
+func main() {
+	test.Hello("use lib test")
+}
---
+++ lib/test/hello.go
@@ -0,0 +1,10 @@
+package test
+
+/**
+1. 通过 go install 来安装编译后的包
+ */
+import "fmt"
+
+func Hello(word string) {
+	fmt.Println(word)
+}
