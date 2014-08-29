07a366c13d21d521644f0329dd3c9fb18df2e1b9
phpor <phpor@sina.cn>
2014-08-29 11:13:16 +0800

    1. 了解类型断言： 可以将一个interface类型的变量x来断言是否某种非interface类型的值，不能反过来检查非interface类型的值是否实现了某个接口

--- app/basic.go
+++ app/basic.go
@@ -5,7 +5,7 @@ package main
 import "fmt"
 
 func main() {
-	swap()
+	typeAssert()
 }
 
 // 交换两个变量的值，就是这么的简单
@@ -13,5 +13,30 @@ func swap() {
 	x, y := 1, 2
 	x, y = y, x
 	fmt.Println(x, y)
+}
+
+// 类型转换
+func typeConvert() {
+	str := "abc"
+	//fmt.Printf("%T, %+v", str.([]byte),  str.([]byte)) // 这样是不对的
+	fmt.Printf("%T, %+v", []byte(str), []byte(str))
+}
+
+type Str string
+
+// 类型断言
+// 注意： 可以将一个interface类型的变量x来断言是否某种非interface类型的值，不能反过来检查非interface类型的值是否实现了某个接口
+// 参考资料： http://blog.csdn.net/kjfcpua/article/details/18667255
+func typeAssert() {
+	var str interface{} = Str("abc")
+	s, _ := str.(Str)
+	fmt.Printf("%T, %+v", s, s)
+}
+
+// 这里有一个返回值的隐式类型转换
+func yinshiTypeConvert() <-chan int {
+	ch := make(chan int, 5)
+	ch <- 1
+	return ch   // 等同于 return <-chan int(ch)
 }
 
--- app/chan.go
+++ app/chan.go
@@ -7,6 +7,34 @@ import (
 )
 
 func main() {
+
+	ch := make(chan int, 10)
+	ch2 := (<-chan int)(ch) // ch2 只读 // 注意: (<-chan int)(ch)  不等于 <- chan int (ch)
+	ch3 := (chan <- int)(ch) // ch3 只写
+	ch3 <- 1
+	// ch2 <- 2  // 只读的chan不能写
+	fmt.Print(<-ch2)
+
+}
+
+func onewayChan() {
+	ch1 := make(chan int, 10)
+	ch2 := make(<-chan int, 10)    // 直接 make一个只读或只写的chan是没有意义的
+	ch3 := make(chan <- int, 10)
+	ch1 <- 1
+	//	ch2 <- 2
+	//	ch3 <- 3
+	//	fmt.Println(<-ch1, <-ch2, <-ch3)
+
+	fmt.Printf("%T\n%T\n%T\n", ch1, ch2, ch3)
+
+}
+func chanchan(ch chan (chan int)) int {
+	ch2 := make(chan int, 10)
+	ch <- ch2
+	return <-ch2
+}
+func first() {
 	ch := make(chan int, 1)
 
 	go func() {
@@ -14,11 +42,11 @@ func main() {
 			ch <- i
 			time.Sleep(time.Millisecond * 10)
 		}
-		close(ch)    // 这里关闭的只是 写 操作（似乎只能关闭写，也没有关闭读的必要），如果不关闭，最终的结果是 死锁
+		close(ch) // 这里关闭的只是 写 操作（似乎只能关闭写，也没有关闭读的必要），如果不关闭，最终的结果是 死锁
 		// 问题： 这里是单个goroutine在写，如果多个goroutine在写一个chan，则谁来关闭？当然可以弄一个计数器，当然还要有锁，麻烦
 	}()
 	for {
-		j, ok := <-ch    // 注意： 不是  j <- ch
+		j, ok := <-ch // 注意： 不是  j <- ch
 		if ok {
 			fmt.Println(j)
 		} else {
