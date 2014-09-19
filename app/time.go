ad2922927d8bba97fdec05da73c0fbf716fa57da
phpor <phpor@sina.cn>
2014-09-19 20:03:58 +0800

    1.  强类型语言定义太麻烦

--- app/time.go
+++ app/time.go
@@ -12,6 +12,7 @@ func main() {
 	fmt.Println("Now as my format: ", time.Now().Format("2006-01-02 15:04:05"))
 	fmt.Println("One hour ago: ", time.Unix(time.Now().Unix()-3600, 0))
 	fmt.Println("One hour ago: ", time.Unix(time.Now().Unix()-3600, 0).Format(time.RFC3339))
-	fmt.Printf("%v", time.Now())
+	fmt.Printf("%v\n", time.Now())
+	fmt.Println(time.Now().Before(time.Now().Add(time.Second)))
 }
 
