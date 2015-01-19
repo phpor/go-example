24be824e908631c23944e3cfbe5b97eedc7e116c
phpor <phpor@sina.cn>
2015-01-19 15:43:15 +0800

    Update udpserver.go
    限制goroutine的数量是个愚蠢的做法

--- app/net/udp/udpserver.go
+++ app/net/udp/udpserver.go
@@ -10,20 +10,14 @@ import (
 	"net/http"
 )
 
-
-type job struct {
-	addr *net.UDPAddr
-	data []byte
-}
-
 var (
-	cntIn int = 0
-	cntOut int = 0
-	cntInErr int = 0
-	cntOutErr int = 0
+	cntIn int
+	cntOut int
+	cntInErr int
+	cntOutErr int
 )
 func main() {
-	addr := flag.String("addr", "localhost:12345", "host:port")
+	addr := flag.String("addr", ":12345", "host:port")
 	flag.Parse()
 	udpAddress, err := net.ResolveUDPAddr("udp4", *addr)
 
@@ -43,9 +37,7 @@ func main() {
 
 	defer conn.Close()
 
-	var jobChan = make(chan *job, 50 * 10000)
-	createProductor(conn, jobChan, 1)
-	createWorker(conn, jobChan, 8)
+	go run(conn)
 	go func() {
 		http.ListenAndServe(":7777", nil)
 	}()
@@ -57,53 +49,41 @@ func showStatus() {
 		fmt.Printf("cntIn: %d  cntOut: %d  cntInerr: %d  cntOutErr: %d cntBlock: %d\n", cntIn, cntOut, cntInErr, cntOutErr, cntIn - cntOut)
 		time.Sleep(2 * time.Second)
 	}
-	
-}
-func createProductor(conn *net.UDPConn, jobChan chan *job, num int) {
-	for num > 0 {
-		go func(conn *net.UDPConn, jobChan chan *job) {
-			for {
-				buf := make([]byte, 256)	//这里要给一个合适的大小，不必太大，对于异常数据可以扔掉
-				n, address, err := conn.ReadFromUDP(buf)
 
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "read fail %s \n" ,err)
-					cntInErr += 1
-					continue
-				}
-				if address == nil {
-					cntInErr += 1
-					fmt.Fprintf(os.Stderr, "address fail %v \n" ,address)
-					continue
-				}
-				if n == 0 {
-					fmt.Fprintf(os.Stderr, "data empty \n" ,err)
-					cntInErr += 1
-					continue
-				}
-				buf = buf[:n]
-				jobChan <- &job{address, buf}
-				cntIn += 1
-			}
-		}(conn, jobChan)
-		num -= 1
-	}
-	
 }
-func createWorker(conn *net.UDPConn, jobChan chan *job, num int) {
-	for num > 0 {
-		go func(conn *net.UDPConn, jobChan chan *job) {
-			for {
-				job := <-jobChan
-				_, err := conn.WriteToUDP(job.data, job.addr)
-				if err != nil {
-					cntOutErr += 1
-					fmt.Fprintf(os.Stderr, "write fail %s \n" ,err)
-					continue
-				}
-				cntOut += 1
-			}
-		}(conn, jobChan)
-		num -= 1
+func run(conn *net.UDPConn) {
+	for {
+		buf := make([]byte, 256)	//这里要给一个合适的大小，不必太大，对于异常数据可以扔掉
+		n, address, err := conn.ReadFromUDP(buf)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "read fail %s \n" ,err)
+			cntInErr++
+			continue
+		}
+		if address == nil {
+			cntInErr++
+			fmt.Fprintf(os.Stderr, "address fail %v \n" ,address)
+			continue
+		}
+		if n == 0 {
+			fmt.Fprintf(os.Stderr, "data empty \n")
+			cntInErr++
+			continue
+		}
+		buf = buf[:n]
+		go func(conn *net.UDPConn, address *net.UDPAddr, buf []byte) {
+			doJob(conn, address, buf)
+		}(conn, address, buf)
+		cntIn++
 	}
+
+}
+func doJob(conn *net.UDPConn, address *net.UDPAddr, buf []byte) {
+	_, err = conn.WriteToUDP(buf, address)
+	if err != nil {
+		cntOutErr++
+		fmt.Fprintf(os.Stderr, "write fail %s \n" ,err)
+	}
+	cntOut++
 }
