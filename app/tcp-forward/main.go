7154eb028a4373f671fe9774a4fabbc0feaf2df3
junjie20 <junjie20@staff.weibo.com>
2021-03-23 11:05:31 +0800

    update

--- app/tcp-forward/main.go
+++ app/tcp-forward/main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	addr := flag.String("addr", ":1234", "[host]:port")
-	cmdline := flag.String("cmd", "", "command")
+	cmdline := flag.String("cmd", "/bin/ls", "command")
 	flag.Parse()
 	command := strings.SplitN(*cmdline, ",,", -1)
 	cmdName := command[0]
@@ -30,7 +30,7 @@ func main() {
 		cmd := exec.Command(cmdName, args...)
 		cmd.Stderr = os.Stderr
 		cmd.Stdout = c
-		cmd.Stdin = c
+		cmd.Stdin = c // 这里如果给c的话，cmd.Run会等到client端把c关掉才会返回，如果给nil就不必client主动关掉了
 		go func() {
 			if err := cmd.Run(); err != nil {
 				println(err.Error())
