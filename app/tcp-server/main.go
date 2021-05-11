04c2dbfddc23083aa8485eb259a118782d030e81
junjie20 <junjie20@staff.weibo.com>
2021-05-11 16:50:01 +0800

    通过关闭连接的方法让Accept() 返回

--- app/tcp-server/main.go
+++ app/tcp-server/main.go
@@ -8,11 +8,22 @@ import (
 
 // golang 监听随机端口
 func main() {
-	listen, err := net.Listen("tcp", ":0")
+	listen, err := net.Listen("tcp", ":9090")
 	if err != nil {
 		fmt.Println("listen error: ", err)
 		return
 	}
-	time.Sleep(100 * time.Second)
+
+	go func() {
+		time.Sleep(3 * time.Second)
+		listen.Close()
+	}()
+	c, err := listen.Accept()
+	if err != nil {
+		println(err.Error())
+	} else {
+		println(c.RemoteAddr())
+	}
+	//time.Sleep(100 * time.Second)
 	println(listen.Addr().String())
 }
