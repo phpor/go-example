f68d4344780b9e59037dea33b5d6dba4806b3ba6
junjie20 <junjie20@staff.weibo.com>
2024-08-29 16:15:05 +0800

    直接使用c的源文件写cgo

--- app/cgo/main.go
+++ app/cgo/main.go
@@ -1,9 +1,20 @@
 package main
 
+/* cgo 的基本步骤
+1. 如果使用外部的库，需要在编译时链接外部库，否则不需要
+2. 通过注释的方式，引入C语言的头文件，或者直接写extern xxx，并紧挨着写import "C"
+*/
+
+/*
+extern int simple(void);
+*/
+import "C"
+
 import (
 	"cgo/sdk"
 )
 
 func main() {
 	sdk.Say()
+	C.simple()
 }
---
+++ app/cgo/simple.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int simple(void) {
+    printf("simple\n"); // 加个换行比较好，否则可能不会输出的哦，没有换行的话，mac上能输出，linux上不输出
+    return 0;
+}
