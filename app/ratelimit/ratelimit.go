f04171662beb53716f6f05c649a0ac22334d54a0
junjie20 <junjie20@staff.weibo.com>
2025-02-05 16:27:21 +0800

    令牌桶算法

--- app/ratelimit/ratelimit.go
+++ app/ratelimit/ratelimit.go
@@ -3,12 +3,18 @@ package main
 import (
 	"context"
 	"golang.org/x/time/rate"
+	"time"
 )
 
 func main() {
 	l := rate.NewLimiter(1, 10)
 	for i := 0; i < 15; i++ {
 		// 10个请求，每秒1个请求，10秒内完成
+		_ = l.Wait(context.Background())
+		println(i)
+	}
+	time.Sleep(3 * time.Second)
+	for i := 0; i < 3; i++ {
 		_ = l.Wait(context.Background())
 		println(i)
 	}
