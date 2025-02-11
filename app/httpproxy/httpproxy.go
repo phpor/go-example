50be2998f6a8d914de269884b2433beb6c8bd04e
junjie20 <junjie20@staff.weibo.com>
2025-02-11 10:24:01 +0800

    支持代理认证

--- app/httpproxy/httpproxy.go
+++ app/httpproxy/httpproxy.go
@@ -3,14 +3,36 @@ package main
 import (
 	"flag"
 	"github.com/elazarl/goproxy"
+	"github.com/elazarl/goproxy/ext/auth"
 	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
+	verbose := flag.Bool("v", false, "should every proxy request be logged to stdout")
+	addr := flag.String("addr", ":8686", "proxy listen address")
+	username := flag.String("username", "", "proxy auth username")
+	password := flag.String("password", "", "proxy auth password")
+	flag.Parse()
+
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.KeepHeader = true
+	//proxy.OnRequest().HandleConnectFunc(func(host string, ctx *goproxy.ProxyCtx) (*goproxy.ConnectAction, string) {
+	//	c := goproxy.OkConnect
+	//	c.TLSConfig = func(host string, ctx *goproxy.ProxyCtx) (*tls.Config, error) {
+	//
+	//	}
+	//	return c, ""
+	//})
+	if username != nil && *username != "" && password != nil && *password != "" {
+		proxy.OnRequest().HandleConnect(auth.BasicConnect("my_realm", func(user, passwd string) bool {
+			return user == *username && passwd == *password
+		}))
+		proxy.OnRequest().Do(auth.Basic("my_realm", func(user, passwd string) bool {
+			return user == *username && passwd == *password
+		}))
+	}
 	proxy.NonproxyHandler = http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		req.URL.Scheme = "http"
 		req.Host = req.Header.Get("Host")
@@ -23,10 +45,8 @@ func main() {
 		proxy.ServeHTTP(w, req)
 		//http.Error(w, "This is a proxy server. Does not respond to non-proxy requests.", 500)
 	})
-	verbose := flag.Bool("v", false, "should every proxy request be logged to stdout")
-	addr := flag.String("addr", ":8686", "proxy listen address")
-	flag.Parse()
 
 	proxy.Verbose = *verbose
+
 	log.Fatal(http.ListenAndServe(*addr, proxy))
 }
